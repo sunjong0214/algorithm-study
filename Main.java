d8250547cb969d95ccf7d768bcf5dd66d423c5c6
sunjong0214 <rlatjswhd8200@gmail.com>
2023-12-04 01:54:30 +0900

    d

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) throws IOException{
@@ -15,13 +14,14 @@ public class Main {
     String[] strTemp = new String[n];
     int[] strLen = new int[n];
     String sTemp = null;
-
-    for (int i=0; i<n; i++) {
+    int i = 0;
+    int j = 0;
+    for (i=0; i<n; i++) {
       str[i] = br.readLine();
       strLen[i] = str[i].length();
     }
-    for (int i=0; i<str.length; i++){
-      for (int j=0; j<str.length; j++) {
+    for (i=0; i<str.length; i++){
+      for ( j=0; j<str.length; j++) {
         if (strLen[i] < strLen[j]) {
           int temp = strLen[j];
           strLen[j] = strLen[i];
@@ -32,35 +32,53 @@ public class Main {
         }
       }
     }
-        for (int i=0; i<str.length; i++){
-          for (int j=1; j<str.length; j++) {
-            if (strLen[i] == strLen[j]) {
-            int charIndex = 0;
-              while (true) {
-                char[] strChar = new char[50];
-                strChar[charIndex] = str[i].charAt(charIndex);
-                char[] strChar2 = new char[50];
-                strChar2[charIndex] = str[j].charAt(charIndex);
-                if (strChar[charIndex] == strChar2[charIndex]) {
-                  charIndex++;
-                  if (str[i].charAt(charIndex) == '\0'){
-                    str[j] = null;
-                  }
-                  break;
-                } else if (strChar[charIndex] < strChar2[charIndex]) {
-                  break;
-                } else if (strChar[charIndex] > strChar2[charIndex]) {
-                  sTemp = str[j];
-                  str[j] = str[i];
-                  str[i] = sTemp;
-                  break;
-                }
-              }
-            }
-        }
+    j = 0;
+    for (i=0; i<str.length; i++) {
+      if (i+2 > str.length){
+        strTemp[j] = str[i];
+        break;
       }
-    for (int i=0; i<n; i++) {
-      bw.write(str[i] + "\n");
+      if (str[i].equals(str[i+1])) {
+        continue;
+      }
+        strTemp[j] = str[i];
+        j++;
+    }
+    int tempIndex = j;
+    j = 0;
+    int check = 0;
+    int checkIndex = 0;
+    for (i=0; i<tempIndex; i++){
+      if (i+2 > tempIndex)
+        break;
+        if (check == 0)
+          checkIndex = i;
+        else if (check == 1)
+          i = checkIndex;
+      if (strTemp[i].length() == strTemp[i+1].length()){
+        
+        for (j=0; j<str[i].length(); ) {
+            if (strTemp[i].charAt(j) > strTemp[i+1].charAt(j)) {
+              String temp = strTemp[i];
+              strTemp[i] = strTemp[i+1];
+              strTemp[i+1] = temp;
+              check = 1;
+              break;
+            } else if (strTemp[i].charAt(j) == strTemp[i+1].charAt(j)) {
+              j++;
+              continue;
+            } else {
+              check = 2;
+              break;
+            }
+          }
+      }
+    }
+    for ( i=0; i<tempIndex+1; i++) {
+      if (strTemp[i] == "1"){
+        continue;
+      }
+      bw.write(strTemp[i] + "\n");
     }
     br.close();
     bw.flush();
