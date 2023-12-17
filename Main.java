4b90dd72ebba8d2196e6a21ae135662bac9bdac5
sunjong0214 <rlatjswhd8200@gmail.com>
2023-12-18 03:46:21 +0900

    d

--- Main.java
+++ Main.java
@@ -10,73 +10,16 @@ public class Main {
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     int n = Integer.parseInt(br.readLine());
-    String[] str = new String[n];
-    String[] strTemp = new String[n];
-    int[] strLen = new int[n];
-    String sTemp = null;
-    int i = 0;
-    int j = 0;
-    for (i=0; i<n; i++) {
-      str[i] = br.readLine();
-      strLen[i] = str[i].length();
-    }
-    for (i=0; i<str.length; i++){
-      for ( j=0; j<str.length; j++) {
-        if (strLen[i] < strLen[j]) {
-          int temp = strLen[j];
-          strLen[j] = strLen[i];
-          strLen[i] = temp;
-          sTemp = str[j];
-          str[j] = str[i];
-          str[i] = sTemp;
-        }
+    int count = 1;
+    int num = 666;
+    while (n != count) {
+      num++;
+      if(String.valueOf(num).contains("666")) {
+        count++;
       }
     }
-    j = 0;
-    for (i=0; i<str.length; i++) {
-      if (i+2 > str.length){
-        strTemp[j] = str[i];
-        break;
-      }
-      if (str[i].equals(str[i+1])) {
-        continue;
-      }
-        strTemp[j] = str[i];
-        j++;
-    }
-    int tempIndex = j;
-    j = 0;
-    int check = 0;
-    int checkIndex = 0;
-    for (i=0; i<tempIndex; i++){
-      if (i+2 > tempIndex)
-        break;
-        if (check == 0)
-          checkIndex = i;
-        else if (check == 1)
-          i = checkIndex;
-      if (strTemp[i].length() == strTemp[i+1].length()){
-        
-        for (j=0; j<str[i].length(); ) {
-            if (strTemp[i].charAt(j) > strTemp[i+1].charAt(j)) {
-              String temp = strTemp[i];
-              strTemp[i] = strTemp[i+1];
-              strTemp[i+1] = temp;
-              check = 1;
-              break;
-            } else if (strTemp[i].charAt(j) == strTemp[i+1].charAt(j)) {
-              j++;
-              continue;
-            } else {
-              check = 2;
-              break;
-            }
-          }
-      }
-    }
-    for ( i=0; i<tempIndex+1; i++) {
-      bw.write(strTemp[i] + "\n");
-    }
+    System.out.println(num);
+
     br.close();
     bw.flush();
     bw.close();
