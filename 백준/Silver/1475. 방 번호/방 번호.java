0b6d7b1969154539e14acf932413791430f38d82
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2023-12-23 02:28:42 +0900

    [Silver V] Title: 방 번호, Time: 148 ms, Memory: 16020 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.Scanner;
+import java.lang.*;
+
+public class Main {
+  public static void main (String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    String n = br.readLine();
+    int[] arr = new int[10];
+    int max = 0;
+    int temp = 0;
+    for (int i=0; i<n.length(); i++) {
+      temp = n.charAt(i);
+      arr[temp-48]++;
+    }
+    max = (arr[6] + arr[9]) / 2;
+    if ((arr[6] + arr[9]) % 2 == 1) {
+      max++;
+    }
+    for (int i=0; i<arr.length; i++) {
+      if (i == 6 || i == 9){
+        continue;
+      }
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+    }
+    bw.write(max + "\n");
+    br.close();
+    bw.flush();
+    bw.close();
+  }
+}
