4da4aac4558336996c80a25643c4971767aa1bd4
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-01-21 23:13:27 +0900

    [Bronze V] Title: 개수 세기, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int x = Integer.parseInt(br.readLine());
+    int result = 0;
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+      if (arr[i] == x) {
+        result++;
+      }
+    }
+    System.out.println(result);
+    br.close();
+  }
+}
