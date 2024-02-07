50cc510cdc2a295ae3c56e9c3758c8f194bb579b
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-02-07 17:55:41 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 120 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    
+    while (true) {
+      String str = br.readLine();
+      if (str.equals("0 0 0")) {
+        break;
+      }
+      StringTokenizer st = new StringTokenizer(str);
+      int x = Integer.parseInt(st.nextToken());
+      int y = Integer.parseInt(st.nextToken());
+      int z = Integer.parseInt(st.nextToken());
+
+      int max = Math.max(Math.max(x, y), z);
+
+      if (max >= x + y || max >= y + z || max >= z + x) {
+        System.out.println("Invalid");
+        continue;
+      }
+      if (x == y && y == z) {
+        System.out.println("Equilateral");
+      } else if (x == y || y == z || z == x) {
+        System.out.println("Isosceles");
+      } else if (x != y && y != z && z != x) {
+        System.out.println("Scalene");
+      }
+    }
+    br.close();
+  }
+}
