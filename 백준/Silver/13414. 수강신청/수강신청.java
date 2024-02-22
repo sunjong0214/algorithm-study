b46dfd9ea3d49a9f07fc0eb883b668599ee5da6d
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-02-22 12:49:56 +0900

    [Silver III] Title: 수강신청, Time: 768 ms, Memory: 67864 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.java
+++ 백준/Silver/13414. 수강신청/수강신청.java
@@ -1,40 +1,35 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.StringTokenizer;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st = new StringTokenizer(br.readLine());
     int n = Integer.parseInt(st.nextToken());
     int m = Integer.parseInt(st.nextToken());
 
-    HashMap<String, Integer> map = new HashMap<>();
-    String[] priority = new String[m];
+    HashSet<String> set = new LinkedHashSet<>();
     for (int i = 0; i < m; i++) {
-      map.put(br.readLine(), i);
+      String str = br.readLine();
+      if (set.contains(str)) {
+        set.remove(str);
+      }
+      set.add(str);
     }
-    List<String> list = new ArrayList<>(map.keySet());
-    for (int i = 0; i < list.size(); i++) {
-      priority[map.get(list.get(i))] = list.get(i);
-    }
-    int i = 0;
-    while (n != 0) {
-      if (i == priority.length) {
+    for (String str : set) {
+      if (n-- == 0) {
         break;
       }
-      if (priority[i] == null) {
-        i++;
-        continue;
-      } else {
-        System.out.println(priority[i]);
-        i++;
-        n--;
-      }
+      bw.write(str + "\n");
     }
+    br.close();
+    bw.close();
   }
 }
