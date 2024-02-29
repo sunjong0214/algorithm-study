5bce5d0501d9e243a25700e6841c4aa446c746d7
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-02-29 17:10:08 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1708 ms, Memory: 57592 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+  static class Coordinate {
+    int x;
+    int y;
+
+    public Coordinate(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    public String toString() {
+      return x + " " + y + "\n";
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+    StringTokenizer st;
+    Coordinate[] cdn = new Coordinate[N];
+
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      cdn[i] = new Coordinate(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+    }
+
+    Arrays.sort(cdn, (c1, c2) -> {
+      int yDiff = c1.y - c2.y;
+      if (yDiff == 0) {
+        return c1.x - c2.x;
+      }
+      return yDiff;
+    });
+
+    for (int i = 0; i < N; i++) {
+      System.out.print(cdn[i].toString());
+    }
+    br.close();
+  }
+}
