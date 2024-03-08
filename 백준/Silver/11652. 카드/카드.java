5ea76c85159c74b59ab911f6714228d65f1a7ba9
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-03-08 16:03:15 +0900

    [Silver IV] Title: 카드, Time: 364 ms, Memory: 31284 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -1,48 +1,29 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     int n = Integer.parseInt(br.readLine());
-    Map<String, Integer> map = new HashMap<>();
-    int cnt = 1;
+    Map<Long, Integer> map = new HashMap<>();
+    int max = 0;
+    long result = 0;
     for (int i = 0; i < n; i++) {
-      String str = br.readLine();
-      if (map.containsKey(str)) {
-        map.put(str, map.get(str) + 1);
-      } else {
-        map.put(str, cnt);
+      Long num = Long.parseLong(br.readLine());
+      map.put(num, map.getOrDefault(num, 0) + 1);
+
+      if (map.get(num) > max) {
+        max = map.get(num);
+        result = num;
+      } else if (map.get(num) == max) {
+        result = Math.min(result, num);
       }
     }
-    List<String> list = new ArrayList<>(map.keySet());
-    String s = list.remove(0);
-    long result = Long.parseLong(s);
-    int min = map.get(s);
-    for (String str : list) {
-      int temp = map.get(str);
-      if (min < temp) {
-        result = Long.parseLong(str);
-        min = temp;
-        continue;
-      }
-      if (min == temp) {
-        if (result  > Long.parseLong(str)) {
-          result = Long.parseLong(str);
-        }
-      }
-    }
-    bw.write(result + "\n");
+    System.out.println(result);
     br.close();
-    bw.close();
   }
 }
