c4123d5a05e79d0e441b4ee5f99fc90064f6c36c
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-03-29 01:48:16 +0900

    [Bronze I] Title: 소인수분해, Time: 144 ms, Memory: 15892 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -5,38 +5,20 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main {
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    int n = Integer.parseInt(br.readLine());
-    if (n != 1 && prime(n) == true) {
-      bw.write(n + "\n");
-      n = 0;
-    }
-    for (int i = 2; i <= n; i++) {
-      if (n % i != 0) {
-        continue;
-      }
-      n /= i;
-      if (prime(i) == true) {
+    int N = Integer.parseInt(br.readLine());
+    for (int i = 2; i * i <= N; i++) {
+      while (N % i == 0) {
+        N /= i;
         bw.write(i + "\n");
       }
-      i = 1;
     }
+    if (N != 1)
+      bw.write(N + "\n");
     br.close();
     bw.close();
-  }
-
-  static boolean prime(int num) {
-    if (num == 2) {
-      return true;
-    }
-    for (int i = 2; i <= Math.sqrt(num); i++) {
-      if (num % i == 0) {
-        return false;
-      }
-    }
-    return true;
   }
 }
