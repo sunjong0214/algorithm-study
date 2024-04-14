de15bb75d296e463019834373c14dbed49e5a00f
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-04-15 00:48:02 +0900

    [Silver IV] Title: 스택, Time: 220 ms, Memory: 20072 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,57 +1,87 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+
+import java.io.*;
+import java.lang.Object;
 import java.util.StringTokenizer;
 
 public class Main {
+
+  public static class MyStack<T> {
+    private Object[] arr;
+    private int size;
+
+    public MyStack() {
+      this.arr = new Object[100001];
+      size = 0;
+    }
+
+    public void push(T item) {
+      arr[size++] = (T) item;
+    }
+
+    public T pop() {
+      if (size == 0) {
+        return null;
+      }
+      size--;
+      T save = (T) arr[size];
+      arr[size] = null;
+      return save;
+    }
+
+    public T peek() {
+      if (size == 0) {
+        return null;
+      }
+      return (T) arr[size-1];
+    }
+
+    public boolean isEmpty() {
+      return size == 0;
+    }
+
+    public int size() {
+      return size;
+    }
+  }
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    int n = Integer.parseInt(br.readLine()); // 량
-    Stack<Integer> stack = new Stack<>();
-    StringBuilder sb = new StringBuilder();
-    while (n-- != 0) {
-      StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-      switch (st.nextToken()) {
-        case "push":
-          stack.push(Integer.parseInt(st.nextToken()));
-          break;
-        case "pop":
-          if (stack.isEmpty()) {
-            sb.append(-1 + "\n");
-            break;
-          }
-          sb.append(stack.pop() + "\n");
-          break;
-        case "size":
-          sb.append(stack.size() + "\n");
-          break;
-        case "empty":
-          if (stack.isEmpty()) {
-            sb.append(1 + "\n");
-            break;
-          }
-          sb.append(0 + "\n");
-          break;
-        case "top":
-          if (stack.isEmpty()) {
-            sb.append(-1 + "\n");
-            break;
-          }
-          sb.append(stack.peek() + "\n");
-          break;
+    MyStack<Integer> stack = new MyStack<>();
+
+    int n = Integer.parseInt(br.readLine());
+    for (int i = 0; i < n; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      String cmd = st.nextToken();
+      if (cmd.equals("push")) {
+        stack.push(Integer.parseInt(st.nextToken()));
+      } else if (cmd.equals("pop")) {
+        Integer temp = stack.pop();
+        if (temp == null) {
+          bw.write(-1 + "\n");
+        } else {
+          bw.write(temp + "\n");
+        }
+      } else if (cmd.equals("size")) {
+        bw.write(stack.size() + "\n");
+      } else if (cmd.equals("empty")) {
+        if (stack.isEmpty()) {
+          bw.write(1 + "\n");
+        } else {
+          bw.write(0 + "\n");
+        }
+      } else {
+        if (stack.peek() == null) {
+          bw.write(-1 + "\n");
+        } else {
+          bw.write(stack.peek() + "\n");
+        }
       }
     }
-    bw.write(sb + "");
     br.close();
     bw.close();
   }
 }
+
+
