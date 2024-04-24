81d641b9831f1f4fc0e51d25cda19dd9169ba5f2
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-04-24 13:47:41 +0900

    [Silver III] Title: 회전하는 큐, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -1,53 +1,72 @@
-
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.IOException;
+ 
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
-
+ 
 public class Main {
-
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    int N = Integer.parseInt(st.nextToken()); // 큐의 크기
-    int M = Integer.parseInt(st.nextToken()); // 뽑을 수의 개수
-    st = new StringTokenizer(br.readLine());
-
-    LinkedList<Integer> q = new LinkedList<>();
-
-    for (int i = 1; i <= N; i++) {
-      q.add(i);
-    }
-
-    int result = 0;
-    for (int i = 0; i < M; i++) {
-      int loc = Integer.parseInt(st.nextToken());
-      int idx = q.indexOf(loc);
-
-      if (idx > q.size() / 2) {
-        for (int j = 0; j < q.size() - idx; j++) {
-          q.addFirst(q.removeLast());
-          result++;
-        }
-      } else {
-        for (int j = 0; j < idx; j++) {
-          q.add(q.remove());
-          result++;
-        }
-      }
-      q.remove();
-      if (q.isEmpty()) {
-        break;
-      }
-    }
-    bw.write(result + "");
-    br.close();
-    bw.close();
-  }
+ 
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		LinkedList<Integer> deque = new LinkedList<Integer>();
+		
+		int count = 0;	// 2, 3번 연산 횟수 누적 합 변수
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());	// 큐의 크기(1 ~ N)
+		int M = Integer.parseInt(st.nextToken());	// 뽑으려는 숫자의 개수
+		
+		// 1부터 N까지 덱에 담아둔다.
+		for(int i = 1; i <= N; i++) {
+			deque.offer(i);
+		}
+		
+		int[] seq = new int[M];	// 뽑고자 하는 수를 담은 배열
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i < M; i++) {
+			seq[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		
+		for(int i = 0; i < M; i++) {
+			
+			int target_idx = deque.indexOf(seq[i]);
+			int half_idx;
+		
+			if(deque.size() % 2 == 0) {
+				half_idx = deque.size() / 2 - 1;
+			}
+			else {
+				half_idx = deque.size() / 2;
+			}
+			
+			
+			// 중간 지점 또는 중간 지점보다 원소의 위치가 앞에 있을 경우
+			if(target_idx <= half_idx) {
+				// idx 보다 앞에 있는 원소들을 모두 뒤로 보낸다. (2번 연산)
+				for(int j = 0; j < target_idx; j++) {
+					int temp = deque.pollFirst();
+					deque.offerLast(temp);
+					count++;
+				}
+			}
+			else {
+				for(int j = 0; j < deque.size() - target_idx; j++) {
+					int temp = deque.pollLast();
+					deque.offerFirst(temp);
+					count++;
+				}
+			
+			}
+			deque.pollFirst();
+		}
+		
+		System.out.println(count);
+		
+		
+	}
 }
