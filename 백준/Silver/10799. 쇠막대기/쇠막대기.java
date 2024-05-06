f8e3d5e680823bf8291a09218b90bbf5ae55e5d5
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-05-07 00:39:28 +0900

    [Silver II] Title: 쇠막대기, Time: 192 ms, Memory: 16360 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,36 +1,33 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+
+import java.io.*;
 import java.util.Stack;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    String input = br.readLine();
-    Stack<Character> stack = new Stack<>();
-    stack.push(input.charAt(0));
-    int pipe = 1;
-    for (int i = 1; i < input.length(); i++) {
-      if (input.charAt(i) == '(') {
-        stack.push(input.charAt(i));
-        pipe++;
-      } else if (input.charAt(i) == ')') {
-        if (input.charAt(i-1) == '(') { // 레이저
-          stack.pop();
-          pipe--;
-          pipe = pipe + stack.size();
-        } else {
-          stack.pop();
+        String str = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        int result = 0;
+        boolean b = true;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ')') {
+                if (stack.peek() == '(' && b) {
+                    stack.pop();
+                    b = false;
+                    result += stack.size();
+                } else {
+                    stack.pop();
+                    result++;
+                }
+            } else {
+                b = true;
+                stack.push(str.charAt(i));
+            }
         }
-      }
+        System.out.println(result);
+        br.close();
+        bw.close();
     }
-    bw.write(pipe + "");
-    br.close();
-    bw.close();
-  }
 }
