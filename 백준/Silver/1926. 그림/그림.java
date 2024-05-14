ed9cfb565a25c0399952c91cdad44796447c1d62
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-05-14 14:37:09 +0900

    [Silver I] Title: 그림, Time: 512 ms, Memory: 49528 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,76 +8,73 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+  static int[][] arr = new int[501][501];
+  static boolean[][] isVisit = new boolean[501][501];
+  static int x;
+  static int y;
+  static Queue<Integer[]> queue = new LinkedList<>();
+  static int[] dx = {1, 0, -1, 0};
+  static int[] dy = {0, 1, 0, -1};
+
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     StringTokenizer st = new StringTokenizer(br.readLine());
-    int x = Integer.parseInt(st.nextToken()); // 행
-    int y = Integer.parseInt(st.nextToken()); // 열
-    int[][] arr = new int[501][501];
-    boolean[][] visited = new boolean[501][501];
+    y = Integer.parseInt(st.nextToken());
+    x = Integer.parseInt(st.nextToken());
 
-    for (int i = 0; i < x; i++) {
-      st = new StringTokenizer(br.readLine());
-      for (int j = 0; j < y; j++) {
-        arr[i][j] = Integer.parseInt(st.nextToken());
+    setArr(br);
+    int[] result = new int[2];
+    for (int i = 0; i < y; i++) {
+      for (int j = 0; j < x; j++) {
+        if (arr[i][j] == 1 && !isVisit[i][j]) {
+          queue.add(new Integer[]{i, j});
+          isVisit[i][j] = true;
+
+          result[0]++;
+          int size = 0;
+          while (!queue.isEmpty()) {
+            int qy = queue.peek()[0];
+            int qx = queue.peek()[1];
+            queue.poll();
+            size++;
+            for (int l = 0; l < 4; l++) {
+              if (qy + dy[l] < 0 || qy + dy[l] >= y) {
+                continue;
+              }
+              if (qx + dx[l] < 0 || qx + dx[l] >= x) {
+                continue;
+              }
+              if (arr[qy + dy[l]][qx + dx[l]] == 1 && !isVisit[qy + dy[l]][qx + dx[l]]) {
+                queue.add(new Integer[]{qy + dy[l], qx + dx[l]});
+                isVisit[qy + dy[l]][qx + dx[l]] = true;
+              }
+            }
+          }
+          if (size > result[1]) {
+            result[1] = size;
+          }
+        }
       }
     }
-    int[] deX = { 1, 0, -1, 0 };
-    int[] deY = { 0, 1, 0, -1 };
-    int count = 0;
-    int maxArea = 0;
-    Queue<int[]> queue = new LinkedList<>();
-    int idxX = 0;
-    int idxY = 0;
-    while (idxX < x || idxY < y) {
-      if (arr[idxX][idxY] == 0 || visited[idxX][idxY] == true) {
-        idxY++;
-        if (idxY > y) {
-          idxX++;
-          idxY = 0;
-        }
-        if (idxX > x) {
-          break;
-        }
-        continue;
-      }
-      queue.offer(new int[] { idxX, idxY });
-      visited[idxX][idxY] = true;
-      int area = 0;
-      area++;
-      idxY++;
-      if (idxY > y) {
-        idxX++;
-        idxY = 0;
-      }
-      if (idxX > x) {
-        break;
-      }
-      while (!queue.isEmpty()) {
-        int qX = queue.peek()[0];
-        int qY = queue.peek()[1];
-        queue.poll();
-        for (int i = 0; i < 4; i++) {
-          int cX = qX + deX[i];
-          int cY = qY + deY[i];
-          if (cX < 0 || cX > 501 || cY < 0 || cY > 501)
-            continue;
-          if (visited[cX][cY] == true)
-            continue;
-          visited[cX][cY] = true;
-          if (arr[cX][cY] == 0)
-            continue;
-          queue.offer(new int[] { cX, cY });
-          area++;
-        }
-      }
-      if (maxArea < area) {
-        maxArea = area;
-      }
-      count++;
+    for (int i : result) {
+      System.out.println(i);
     }
-    System.out.println(count + "\n" + maxArea);
     br.close();
+  }
+
+  private static void setArr(BufferedReader br) throws IOException {
+    StringTokenizer st;
+    for (int i = 0; i < y; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < x; j++) {
+        int num = Integer.parseInt(st.nextToken());
+        arr[i][j] = num;
+        if (num == 0) {
+          isVisit[i][j] = true;
+        }
+      }
+    }
   }
 }
