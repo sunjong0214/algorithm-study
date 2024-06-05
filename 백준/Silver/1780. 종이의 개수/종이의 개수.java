d3ba6c63b996e6799e032bf4a88225cc658ad945
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-06-05 19:19:54 +0900

    [Silver II] Title: 종이의 개수, Time: 844 ms, Memory: 310472 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -4,50 +4,54 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-  static int[][] arr;
-  static int[] num = new int[3];
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[][] arr;
+    static int[] result = new int[3];
+    static int N;
 
-    int n = Integer.parseInt(br.readLine());
-    arr = new int[n][n];
-    for (int i = 0; i < n; i++) {
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      for (int j = 0; j < n; j++) {
-        arr[i][j] = Integer.parseInt(st.nextToken());
-      }
-    }
-    func(n, 0, 0);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    for (int z : num) {
-      System.out.println(z);
-    }
-
-    br.close();
-  }
-
-  static boolean paper(int idx, int x, int y) {
-    for (int i = x; i < x + idx; i++) {
-      for (int j = y; j < y + idx; j++) {
-        if (arr[x][y] != arr[i][j]) {
-          return false;
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
-      }
-    }
-    return true;
-  }
 
-  static void func(int idx, int x, int y) {
-    if (paper(idx, x, y)) {
-      num[arr[x][y] + 1]++;
-      return;
+        func(0, 0, N);
+
+        for (int x : result) {
+            System.out.println(x);
+        }
+        br.close();
     }
-    idx /= 3;
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 3; j++) {
-        func(idx, x + i * idx, y + j * idx);
-      }
+
+    static void func(int y, int x, int n) {
+        if (check(y, x, n)) {
+            result[arr[y][x] + 1]++;
+            return;
+        }
+
+        int index = n / 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                func(y + i * index, x + j * index, index);
+            }
+        }
     }
-  }
+
+    private static boolean check(int y, int x, int n) {
+        int s = arr[y][x];
+        for (int i = y; i < y + n; i++) {
+            for (int j = x; j < x + n; j++) {
+                if (arr[i][j] != s) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
