e3fbb2f2c1964131c124749e482464470d737186
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-09-24 01:21:03 +0900

    [Gold V] Title: 탑, Time: 844 ms, Memory: 101440 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,80 +1,43 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Stack;
 import java.util.StringTokenizer;
+
+import org.w3c.dom.Node;
 
 public class Main {
 
-  public static class MyStack<T> {
-
-    private Object[] arr;
-    private int size;
-
-    public MyStack() {
-      this.arr = new Object[500001];
-      size = 0;
-    }
-
-    public void push(T item) {
-      arr[size++] = (T) item;
-    }
-
-    public T pop() {
-      if (size == 0) {
-        return null;
-      }
-      size--;
-      T save = (T) arr[size];
-      arr[size] = null;
-      return save;
-    }
-
-    public T peek() {
-      if (size == 0) {
-        return null;
-      }
-      return (T) arr[size - 1];
-    }
-
-    public boolean isEmpty() {
-      return size == 0;
-    }
-
-    public int size() {
-      return size;
-    }
-  }
-
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    MyStack<int[]> stack = new MyStack<>();
-
-    int N = Integer.parseInt(br.readLine());
-
+    int n = Integer.parseInt(br.readLine());
+    Stack<int[]> stack = new Stack<>();
     StringTokenizer st = new StringTokenizer(br.readLine());
-    int top = 1;
-    while (st.hasMoreTokens()) {
-      int[] num = new int[] {Integer.parseInt(st.nextToken()), top++};
-      while (true) {
-        if (stack.peek() != null && num[0] > stack.peek()[0]) {
-          stack.pop();
-        } else {
-          int[] temp = stack.peek();
-          if (temp == null) {
-            bw.write(0 + " ");
-          } else {
-            bw.write(stack.peek()[1] + " ");
-          }
-          stack.push(num);
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 1; i <= n; i++) {
+      int tower = Integer.parseInt(st.nextToken());
+      if (stack.isEmpty()) {
+        sb.append("0 ");
+      }
+      while (!stack.isEmpty()) {
+        if (stack.peek()[1] > tower) {
+          sb.append(stack.peek()[0] + " ");
           break;
+        } else {
+          stack.pop();
+          if (stack.isEmpty()) {
+            sb.append("0 ");
+            break;
+          }
         }
       }
+      stack.push(new int[] {i, tower});
     }
+    sb.deleteCharAt(sb.length() - 1);
+    System.out.println(sb);
     br.close();
-    bw.close();
   }
 }
