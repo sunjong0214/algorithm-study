91c29e7e950d3b08ce1ba21d97b05900cf785609
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-09-24 14:51:46 +0900

    [Gold IV] Title: 오큰수, Time: 988 ms, Memory: 180448 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,41 +1,39 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    Stack<Integer[]> stack = new Stack<>();
-    int N = Integer.parseInt(br.readLine());
-    Integer[][] arr = new Integer[N][1];
-    StringTokenizer st = new StringTokenizer(br.readLine());
-
-    for (int i = 0; i < N; i++) {
-      arr[i][0] = Integer.parseInt(st.nextToken());
-      if (stack.isEmpty()) {
-        stack.push(arr[i]);
-        continue;
-      }
-      while (!stack.isEmpty() && stack.peek()[0] < arr[i][0]) {
-        stack.pop()[0] = arr[i][0];
-      }
-      stack.push(arr[i]);
+    /**
+     * 만약 입력값이 크면 오큰수가 정해진 값은 pop 하면서 출력
+     * 입력값이 작으면 push 후 오큰수가 나타날 때까지 대기
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Stack<int[]> stack = new Stack<>();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            int[] num = new int[]{Integer.parseInt(st.nextToken()), i};
+            if (!stack.isEmpty() && num[0] > stack.peek()[0]) {
+                while (!stack.isEmpty()) {
+                    if (num[0] <= stack.peek()[0]) {
+                        break;
+                    }
+                    arr[stack.pop()[1]] = num[0];
+                }
+            }
+            stack.push(num);
+        }
+        while (!stack.isEmpty()) {
+            arr[stack.pop()[1]] = -1;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i : arr) {
+            sb.append(i + " ");
+        }
+        bw.write(sb + "");
+        br.close();
+        bw.close();
     }
-
-    while (!stack.isEmpty()) {
-      stack.pop()[0] = -1;
-    }
-    for (int i=0; i<N; i++) {
-      bw.write(arr[i][0] + " ");
-    }
-    br.close();
-    bw.close();
-  }
 }
