5606d81027034f778e71f8b38f21c2e10d454fb0
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-09-24 13:58:11 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 264 ms, Memory: 25648 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -1,69 +1,28 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static class MyStack<T> {
-        private Object[] arr;
-        private int size;
-
-        public MyStack() {
-            this.arr = new Object[100001];
-            size = 0;
-        }
-
-        public T[] arr() {
-            return (T[]) arr;
-        }
-
-        public void push(T item) {
-            arr[size++] = (T) item;
-        }
-
-        public T pop() {
-            if (size == 0) {
-                return null;
-            }
-            size--;
-            T save = (T) arr[size];
-            arr[size] = null;
-            return save;
-        }
-
-        public T peek() {
-            if (size == 0) {
-                return null;
-            }
-            return (T) arr[size-1];
-        }
-
-        public boolean isEmpty() {
-            return size == 0;
-        }
-
-        public int size() {
-            return size;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        MyStack<Long[]> stack = new MyStack<>();
-
-        int N = Integer.parseInt(br.readLine());
-
+        long n = Integer.parseInt(br.readLine());
+        Stack<Long> stack = new Stack<>();
         long sum = 0L;
-        for (int i = 0; i < N; i++) {
-            long mark = 0L;
-            long num = Long.parseLong(br.readLine());
-            while (!stack.isEmpty()) {
-                if (stack.peek()[0] > num) {
-                    mark = stack.peek()[1] + 1;
-                    sum += mark;
-                    break;
+        for (long i = 0L; i < n; i++) {
+            long building = Long.parseLong(br.readLine());
+            if (!stack.isEmpty() && stack.peek() > building) {
+                sum += stack.size();
+            } else {
+                while (!stack.isEmpty()) {
+                    if (stack.peek() > building) {
+                        sum += stack.size();
+                        break;
+                    }
+                    stack.pop();
                 }
-                stack.pop();
             }
-            stack.push(new Long[] {num, mark});
+            stack.push(building);
         }
         bw.write(sum + "");
         br.close();
