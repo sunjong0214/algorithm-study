4226318f6aa21b809d4d03fbd1efd2151f380a42
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-09-25 22:56:01 +0900

    [Silver III] Title: 회전하는 큐, Time: 104 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -1,72 +1,55 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
- 
-import java.util.LinkedList;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		LinkedList<Integer> deque = new LinkedList<Integer>();
-		
-		int count = 0;	// 2, 3번 연산 횟수 누적 합 변수
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());	// 큐의 크기(1 ~ N)
-		int M = Integer.parseInt(st.nextToken());	// 뽑으려는 숫자의 개수
-		
-		// 1부터 N까지 덱에 담아둔다.
-		for(int i = 1; i <= N; i++) {
-			deque.offer(i);
-		}
-		
-		int[] seq = new int[M];	// 뽑고자 하는 수를 담은 배열
-		
-		st = new StringTokenizer(br.readLine(), " ");
-		for(int i = 0; i < M; i++) {
-			seq[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		
-		for(int i = 0; i < M; i++) {
-			
-			int target_idx = deque.indexOf(seq[i]);
-			int half_idx;
-		
-			if(deque.size() % 2 == 0) {
-				half_idx = deque.size() / 2 - 1;
-			}
-			else {
-				half_idx = deque.size() / 2;
-			}
-			
-			
-			// 중간 지점 또는 중간 지점보다 원소의 위치가 앞에 있을 경우
-			if(target_idx <= half_idx) {
-				// idx 보다 앞에 있는 원소들을 모두 뒤로 보낸다. (2번 연산)
-				for(int j = 0; j < target_idx; j++) {
-					int temp = deque.pollFirst();
-					deque.offerLast(temp);
-					count++;
-				}
-			}
-			else {
-				for(int j = 0; j < deque.size() - target_idx; j++) {
-					int temp = deque.pollLast();
-					deque.offerFirst(temp);
-					count++;
-				}
-			
-			}
-			deque.pollFirst();
-		}
-		
-		System.out.println(count);
-		
-		
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        Deque<Integer> dq = new ArrayDeque<>();
+        for (int i = 1; i <= n; i++) {
+            dq.offer(i);
+        }
+        st = new StringTokenizer(br.readLine());
+        int result = 0;
+        for (int i = 0; i < m; i++) {
+            int index = 0;
+            int num = Integer.parseInt(st.nextToken());
+            Object[] array = dq.toArray();
+            for (int j = 0; j < array.length; j++) {
+                if (array[j].equals(num)) {
+                    index = j;
+                    break;
+                }
+            }
+            int size = dq.size();
+            if (size / 2 < index) {
+                // 뒤부터
+                while (true) {
+                    if (dq.peekFirst() == num) {
+                        dq.removeFirst();
+                        break;
+                    }
+                    dq.addFirst(dq.removeLast());
+                    result++;
+                }
+            } else {
+                // 앞부터
+                while (true) {
+                    if (dq.peekFirst() == num) {
+                        dq.removeFirst();
+                        break;
+                    }
+                    dq.addLast(dq.removeFirst());
+                    result++;
+                }
+            }
+        }
+        bw.write(result + "");
+        br.close();
+        bw.close();
+    }
 }
