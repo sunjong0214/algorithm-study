467849890475328933d2e7dd18c27f2bce045e3f
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-10-03 20:45:00 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 104 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,41 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    String str = br.readLine();
-    int result = 0;
-    for (int i = 0; i < str.length(); i++) {
-      char x = str.charAt(i);
-      if (i + 1 < str.length() && (x == 'c' || x == 's' || x == 'z')) {
-        if (str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-') {
-          i++;
-          result++;
-          continue;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        int result = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == 'c') {
+                if (i + 1 < str.length() && (str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-')) {
+                    i++;
+                }
+            } else if (c == 'd') {
+                if (i + 1 < str.length() && str.charAt(i + 1) == '-') {
+                    i++;
+                } else if (i + 2 < str.length() && str.charAt(i + 1) == 'z' && str.charAt(i + 2) == '=') {
+                    i += 2;
+                }
+            } else if (c == 'l') {
+                if (i + 1 < str.length() && str.charAt(i + 1) == 'j') i++;
+            } else if (c == 'n') {
+                if (i + 1 < str.length() && str.charAt(i + 1) == 'j') i++;
+            } else if (c == 's') {
+                if (i + 1 < str.length() && str.charAt(i + 1) == '=') i++;
+            } else if (c == 'z') {
+                if (i + 1 < str.length() && str.charAt(i + 1) == '=') i++;
+            }
+            result++;
         }
-      } else if (i + 1 < str.length() && x == 'd') {
-        if (str.charAt(i + 1) == '-') {
-          i++;
-          result++;
-          continue;
-        } else if (i + 2 < str.length() && (str.charAt(i + 1) == 'z' && str.charAt(i + 2) == '=')) {
-          i = i + 2;
-          result++;
-          continue;
-        }
-      } else if (i + 1 < str.length() && (x == 'n' || x == 'l')) {
-        if (str.charAt(i + 1) == 'j') {
-          i++;
-          result++;
-          continue;
-        }
-      }
-      result++;
+        bw.write(result + "");
+        br.close();
+        bw.close();
     }
-    System.out.println(result);
-    br.close();
-  }
 }
