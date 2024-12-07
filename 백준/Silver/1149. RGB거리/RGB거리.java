8cfca62a60d2e9cc7441d6c3e501d2bc4a04bfdf
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-12-08 07:32:06 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14792 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -6,39 +6,33 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-  static int[][] D;
-  static int[][] house;
-  static int N;
+    static int[][] arr;
+    static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    N = Integer.parseInt(br.readLine());
-    D = new int[N + 5][3];
-    house = new int[N + 5][3];
-    StringTokenizer st;
-    for (int i = 0; i < N; i++) {
-      st = new StringTokenizer(br.readLine());
-      house[i][0] = Integer.parseInt(st.nextToken());
-      house[i][1] = Integer.parseInt(st.nextToken());
-      house[i][2] = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][2] = Integer.parseInt(st.nextToken());
+        }
+        func(1);
+        bw.write(Math.min(arr[n - 1][0], Math.min(arr[n - 1][1], arr[n - 1][2])) + "");
+        br.close();
+        bw.close();
     }
-    D[0][0] = house[0][0];
-    D[0][1] = house[0][1];
-    D[0][2] = house[0][2];
-    func(1);
-    bw.write(Math.min(D[N - 1][0], Math.min(D[N - 1][1], D[N - 1][2])) + "\n");
-    br.close();
-    bw.close();
-  }
 
-  static void func(int x) {
-    if (x == N) {
-      return;
+    private static void func(int index) {
+        if (index == n) {
+            return;
+        }
+        arr[index][0] += Math.min(arr[index - 1][1], arr[index - 1][2]);
+        arr[index][1] += Math.min(arr[index - 1][0], arr[index - 1][2]);
+        arr[index][2] += Math.min(arr[index - 1][0], arr[index - 1][1]);
+        func(++index);
     }
-    D[x][0] = Math.min(D[x - 1][1], D[x - 1][2]) + house[x][0];
-    D[x][1] = Math.min(D[x - 1][0], D[x - 1][2]) + house[x][1];
-    D[x][2] = Math.min(D[x - 1][0], D[x - 1][1]) + house[x][2];
-    func(x + 1);
-  }
 }
