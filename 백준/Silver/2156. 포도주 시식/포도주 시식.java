beb72c38110ea62a023770d9f2797a5455d243d2
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2024-12-14 10:08:12 +0900

    [Silver I] Title: 포도주 시식, Time: 128 ms, Memory: 15364 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -11,28 +11,22 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
 
-        int[] seq = new int[n];
-        int[][] dp = new int[n][3];
+        int[] seq = new int[n + 1];
+        int[] dp = new int[n + 1];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n + 1; i++) {
             seq[i] = Integer.parseInt(br.readLine());
         }
 
-        dp[0][0] = seq[0];
-        dp[0][1] = seq[0];
-        dp[0][2] = 0;
-
-        int m = 0;
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = dp[i - 1][1] + seq[i];
-            dp[i][1] = dp[i - 1][2] + seq[i];
-            dp[i][2] = Math.max(m, Math.max(dp[i - 1][0], dp[i - 1][1]));
-            m = dp[i][2];
+        dp[1] = seq[1];
+        if (n != 1) {
+            dp[2] = Math.max(dp[1], Math.max(dp[1] + seq[2], dp[0] + seq[2]));
+            for (int i = 3; i < n + 1; i++) {
+                dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + seq[i], dp[i - 3] + seq[i - 1] + seq[i]));
+            }
         }
 
-        int max = Math.max(dp[n - 1][0], Math.max(dp[n - 1][1], dp[n - 1][2]));
-
-        bw.write(max + "");
+        bw.write(dp[n] + "");
         br.close();
         bw.close();
     }
