65adbca71725a7be7fd7ce25b138158005611409
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2025-01-06 22:12:40 +0900

    [Gold IV] Title: 타일 채우기, Time: 112 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        dp = new int[n + 1];
+        dp[0] = 1;
+        for (int i = 2; i <= n; i += 2) {
+            dp[i] = dp[i - 2] * 3;
+            for (int j = i - 4; j >= 0; j -= 2) {
+                dp[i] += dp[j] * 2;
+            }
+        }
+        bw.write(dp[n] + "");
+        br.close();
+        bw.close();
+    }
+}
