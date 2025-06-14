be1298be7d53e358d8d928e26c2288761c76a52f
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2025-06-14 09:46:04 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 124 ms, Memory: 16416 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    static int result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[10001][4];
+
+        dp[1][1] = 1;
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+
+        for (int i = 4; i < 10001; i++) {
+            dp[i][1] = dp[i - 1][1];
+            dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+            dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
+        }
+
+        for (int j = 0; j < n; j++) {
+            int num = Integer.parseInt(br.readLine());
+            bw.write(dp[num][1] + dp[num][2] + dp[num][3] + "\n");
+        }
+
+        br.close();
+        bw.close();
+    }
+}
