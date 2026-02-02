1ab76da2c2e76d884e8ae372ea16a9a0ff2bb96c
sunjong0214 <117134728+sunjong0214@users.noreply.github.com>
2026-02-02 09:43:16 +0900

    [Silver II] Title: 이동하기, Time: 432 ms, Memory: 75168 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 class Main {
     public static void main(String[] args) throws IOException {
@@ -11,19 +12,13 @@ class Main {
         String[] input = br.readLine().split(" ");
         int n = Integer.parseInt(input[0]);
         int m = Integer.parseInt(input[1]);
-        int[][] arr = new int[n + 1][m + 1];
         int[][] dp = new int[n + 1][m + 1];
 
+        StringTokenizer st;
         for (int i = 1; i <= n; i++) {
-            String[] ele = br.readLine().split(" ");
+            st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= m; j++) {
-                arr[i][j] = Integer.parseInt(ele[j - 1]);        
-            }
-        }
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                dp[i][j] = arr[i][j] + Math.max(dp[i - 1][j], Math.max(dp[i][j - 1], dp[i - 1][j - 1])); 
+                dp[i][j] = Integer.parseInt(st.nextToken()) + Math.max(dp[i - 1][j], Math.max(dp[i][j - 1], dp[i - 1][j - 1]));;        
             }
         }
 
